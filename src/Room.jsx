653ff9db224ddd1ebe623789a6d14290fd498dc2
theringsofsaturn <emiliankasemi@gmail.com>
2023-06-12 08:28:30 +0200

    Manipulate the nodes of the 3d objects within the scene

--- src/Room.jsx
+++ src/Room.jsx
@@ -1,18 +1,55 @@
 import React, { useLayoutEffect, useRef } from 'react';
-import { useGLTF } from '@react-three/drei'; // useGLTF is a hook that loads a glTF model and returns it with all the materials, textures, and animations attached to it.
+import { useThree, useFrame } from '@react-three/fiber';
+import { useGLTF, useAnimations } from '@react-three/drei'; // useGLTF is a hook that loads a glTF model and returns it with all the materials, textures, and animations attached to it.
 import useGUI from './useGUI';
 
 // Will use Room to dynamically render the room based on the path, position, rotation, and scale props passed to it.
 const Room = ({ path, position, rotation, scale }) => {
   const gltf = useGLTF(path, true); // The boolean true is passed to the useGLTF hook to enable draco compression. Draco is a library that compresses 3D models, which reduces the file size of the 3D model. This is useful when we have a large 3D model, which can take a long time to load. The draco compression reduces the file size of the 3D model, which reduces the loading time of the 3D model.
   console.log(gltf.scene);
+
+  // Destructure animations and nodes from gltf
+  const { animations, nodes } = gltf;
+
+  // Pass animations to useAnimations hook
+  const { actions } = useAnimations(animations);
+
   const ref = useRef();
+  const meshRef = useRef();
+
   useLayoutEffect(() => {
     // it is used to avoid the flickering of the GUI. The flickering is caused by the fact that the GUI is created after the component is rendered. This is because the GUI is created in the useEffect hook, which is called after the component is rendered. To avoid this, we can use the useLayoutEffect hook, which is called before the component is rendered.
     ref.current = gltf.scene; // The gltf.scene object is assigned to the ref.current object.
-  }, [gltf.scene]); // The useEffect hook is called every time the gltf.scene object changes. This happens when the glTF model is loaded.
+  }, [gltf.scene]); // Depend on gltf.scene
 
   useGUI(ref); // The useGUI hook is used to create the GUI for the glTF model. It takes the ref object as an argument.
+
+  useLayoutEffect(() => {
+    // If there are animations, play the first one
+    if (actions && Object.keys(actions).length > 0) {
+      actions[Object.keys(actions)[0]].play();
+    }
+  }, [actions]); // Depend on actions
+
+  useLayoutEffect(() => {
+    // Assuming "ObjectName" is the name of an object in our GLTF model
+    meshRef.current = nodes.ObjectName; // TODO: Find the proper name of the object
+  }, [nodes]); // Depend on nodes
+
+  const { raycaster, camera, scene, render } = useThree();
+
+  useFrame(() => {
+    if (meshRef.current) {
+      const intersections = raycaster.intersectObject(meshRef.current); // will return an array of intersections where the ray from the user's mouse intersects the object. If the user's mouse is over the object, it will return an array with at least one element. If the mouse isn't over the object, it will return an empty array. So if the array's length is greater than zero, we know the user is hovering over the object and we change its color.
+
+      if (intersections.length > 0) {
+        meshRef.current.material.color.set('red');
+      } else {
+        meshRef.current.material.color.set('white');
+      }
+      render(scene, camera);
+    }
+  });
 
   return (
     <primitive // The primitive component is used to render a primitive object
