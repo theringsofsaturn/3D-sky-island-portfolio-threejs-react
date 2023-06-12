b5154ad51d63ea90e1038cc58cc05ca6dea1cde9
theringsofsaturn <emiliankasemi@gmail.com>
2023-06-12 05:21:33 +0200

    comments

--- src/Room.jsx
+++ src/Room.jsx
@@ -1,25 +1,28 @@
 import React, { useLayoutEffect, useRef } from 'react';
-import { useGLTF } from '@react-three/drei';
+import { useGLTF } from '@react-three/drei'; // useGLTF is a hook that loads a glTF model and returns it with all the materials, textures, and animations attached to it.
 import useGUI from './useGUI';
 
+// Will use Room to dynamically render the room based on the path, position, rotation, and scale props passed to it.
 const Room = ({ path, position, rotation, scale }) => {
-  const gltf = useGLTF(path, true);
+  const gltf = useGLTF(path, true); // The boolean true is passed to the useGLTF hook to enable draco compression. Draco is a library that compresses 3D models, which reduces the file size of the 3D model. This is useful when we have a large 3D model, which can take a long time to load. The draco compression reduces the file size of the 3D model, which reduces the loading time of the 3D model.
   console.log(gltf.scene);
   const ref = useRef();
   useLayoutEffect(() => {
-    ref.current = gltf.scene;
-  }, [gltf.scene]);
+    // it is used to avoid the flickering of the GUI. The flickering is caused by the fact that the GUI is created after the component is rendered. This is because the GUI is created in the useEffect hook, which is called after the component is rendered. To avoid this, we can use the useLayoutEffect hook, which is called before the component is rendered.
+    ref.current = gltf.scene; // The gltf.scene object is assigned to the ref.current object.
+  }, [gltf.scene]); // The useEffect hook is called every time the gltf.scene object changes. This happens when the glTF model is loaded.
 
-  useGUI(ref);
+  useGUI(ref); // The useGUI hook is used to create the GUI for the glTF model. It takes the ref object as an argument.
 
   return (
-    <primitive
-      ref={ref}
+    <primitive // The primitive component is used to render a primitive object
+      ref={ref} // The ref object is passed to the primitive component
       object={gltf.scene}
       position={position}
       rotation={rotation}
       scale={scale}
-      dispose={null}
+      dispose={null} // Disposing is a process that removes the model from the scene and frees up memory.
+      // The dispose prop is used to prevent the glTF model from being disposed of when the component is unmounted. This is because the glTF model is loaded asynchronously, and if the component is unmounted before the model is loaded, the model will be disposed of. This will cause the model to disappear from the scene.
     />
   );
 };
--- src/useGUI.jsx
+++ src/useGUI.jsx
@@ -3,19 +3,21 @@ import GUI from 'lil-gui';
 
 const useGUI = (ref) => {
   useEffect(() => {
-    const gui = new GUI();
-    document.body.appendChild(gui.domElement);
+    const gui = new GUI(); // The GUI class is used to create a GUI object.
+
+    // Move the GUI to the bottom right corner of the screen.
+    document.body.appendChild(gui.domElement); 
 
     gui.domElement.style.position = 'fixed';
     gui.domElement.style.right = '0px';
     gui.domElement.style.top = 'auto';
     gui.domElement.style.bottom = '0px';
 
-    const folder = gui.addFolder('Transform');
-    let control;
+    const folder = gui.addFolder('Transform'); // The addFolder method is used to create a folder in the GUI object.
+    let control; // The control variable is used to store the control object returned by the add method.
 
-    control = folder.add(ref.current.position, 'x', -50, 50);
-    control.name('Position X');
+    control = folder.add(ref.current.position, 'x', -50, 50); // The add method is used to add a control to the folder. The add method takes three arguments: the object to which the control is added, the name of the property to which the control is added, and the minimum and maximum values of the control.
+    control.name('Position X'); // The name method is used to set the name of the control.
 
     control = folder.add(ref.current.position, 'y', -50, 50);
     control.name('Position Y');
@@ -23,7 +25,7 @@ const useGUI = (ref) => {
     control = folder.add(ref.current.position, 'z', -50, 50);
     control.name('Position Z');
 
-    control = folder.add(ref.current.rotation, 'x', -Math.PI, Math.PI);
+    control = folder.add(ref.current.rotation, 'x', -Math.PI, Math.PI); // -Math.PI and Math.PI are used to set the minimum and maximum values of the control to -π and π, respectively. This is because the rotation values are in radians. Which means that the minimum and maximum values of the control are -180° and 180°, respectively.
     control.name('Rotation X');
 
     control = folder.add(ref.current.rotation, 'y', -Math.PI, Math.PI);
@@ -41,10 +43,10 @@ const useGUI = (ref) => {
     control = folder.add(ref.current.scale, 'z', 0, 10);
     control.name('Scale Z');
 
-    folder.open();
+    folder.close(); // The close method is used to close the folder, which means that the folder will be collapsed when the GUI is created. You can also use the open method to open the folder, which means that the folder will be expanded when the GUI is created.
 
-    return () => gui.destroy();
-  }, [ref]);
+    return () => gui.destroy(); // The destroy method is used to destroy the GUI object when the component is unmounted. This is to prevent memory leaks.
+  }, [ref]); // The useEffect hook is called every time the ref object changes. This happens when the glTF model is loaded.
 };
 
 export default useGUI;
