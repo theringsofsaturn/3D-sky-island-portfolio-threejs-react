cfff8aa175958ac8fa2ab9ae5ea0d78b0c5db28d
theringsofsaturn <emiliankasemi@gmail.com>
2023-06-12 04:16:22 +0200

    Converted styled components to Tailwind

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,56 +1,30 @@
 // Navbar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
 import { BiHomeCircle } from 'react-icons/bi';
-
-const NavbarContainer = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 60px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 50px;
-  background: rgba(0, 0, 0, 0.8);
-  z-index: 10;
-`;
-
-const Logo = styled.h1`
-  color: #fff;
-  cursor: pointer;
-`;
-
-const NavLinks = styled.div`
-  display: flex;
-  gap: 20px;
-`;
-
-const NavLink = styled(Link)`
-  color: #fff;
-  text-decoration: none;
-
-  &:hover {
-    color: #ddd;
-  }
-`;
 
 const Navbar = () => {
   return (
-    <NavbarContainer>
-      <Logo>
+    <div className="fixed top-0 left-0 w-full h-16 flex justify-between items-center px-12 bg-black bg-opacity-80 z-10">
+      <h1 className="text-white cursor-pointer flex items-center gap-2">
         <BiHomeCircle size={30} />
         3D Portfolio
-      </Logo>
-      <NavLinks>
-        <NavLink to="/room1">Room 1</NavLink>
-        <NavLink to="/room2">Room 2</NavLink>
-        <NavLink to="/room3">Room 3</NavLink>
-        <NavLink to="/room4">Room 4</NavLink>
-      </NavLinks>
-    </NavbarContainer>
+      </h1>
+      <div className="flex gap-8 overflow-auto">
+        <Link to="/room1" className="text-white hover:text-gray-300">
+          Room 1
+        </Link>
+        <Link to="/room2" className="text-white hover:text-gray-300">
+          Room 2
+        </Link>
+        <Link to="/room3" className="text-white hover:text-gray-300">
+          Room 3
+        </Link>
+        <Link to="/room4" className="text-white hover:text-gray-300">
+          Room 4
+        </Link>
+      </div>
+    </div>
   );
 };
 
