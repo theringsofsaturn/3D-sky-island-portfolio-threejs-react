ac866baa4c15b19186a1176b3d4af5012cb7212b
theringsofsaturn <emiliankasemi@gmail.com>
2023-06-15 01:38:48 +0200

    initial camera position fixes

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,18 @@ import './App.css';
 function App() {
   const [controlsEnabled, setControlsEnabled] = useState(true);
   const [modalContent, setModalContent] = useState(null);
+  const [currentStep, setCurrentStep] = useState(0);
 
   return (
     <Router>
       <Navbar />
       <Canvas camera={{ near: 0.1, far: 1000 }}>
         <Suspense fallback={<Loader />}>
-          {/* <OrbitControls
+          <OrbitControls
             enabled={controlsEnabled}
             minDistance={0.1}
             maxDistance={100}
-          /> */}
+          />
           <ambientLight intensity={0.3} />
           <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
           <pointLight position={[-10, -10, -10]} />
@@ -31,11 +32,12 @@ function App() {
               path="/"
               element={
                 <Model
-                  position={[0.5, -1, 1]}
-                  rotation={[0, 0.2, 0]}
+                  position={[0, -0.5, 1]}
+                  rotation={[0.5, 0.3, 0]}
                   scale={[1, 1, 1]}
                   setControlsEnabled={setControlsEnabled}
                   setModalContent={setModalContent}
+                  currentStep={currentStep}
                 />
               }
             />
@@ -45,6 +47,7 @@ function App() {
       {modalContent && (
         <Modal content={modalContent} onClose={() => setModalContent(null)} />
       )}
+      <button onClick={() => setCurrentStep(currentStep + 1)}>Next</button>
     </Router>
   );
 }
