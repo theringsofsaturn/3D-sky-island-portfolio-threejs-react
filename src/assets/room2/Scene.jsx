9112653e1c708e8e3378f6e888d626b1d14b48e3
theringsofsaturn <emiliankasemi@gmail.com>
2023-06-14 20:21:52 +0200

    the correct vector.set for the camera not to jitter

--- src/assets/room2/Scene.jsx
+++ src/assets/room2/Scene.jsx
@@ -33,7 +33,7 @@ export function Model({ setControlsEnabled, ...props }) {
   useFrame((state) => {
     if (clickedMesh) {
       state.camera.lookAt(clickedMesh.position);
-      vec.set(0, 0, -0.08);
+      vec.set(0, 0, -0.02);
       vec.applyQuaternion(state.camera.quaternion);
       target.copy(clickedMesh.position).add(vec);
 
