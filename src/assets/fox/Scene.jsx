b62f57e958a0de73b0bd16506879c8b25e2999f2
theringsofsaturn <emiliankasemi@gmail.com>
2023-07-07 03:45:12 +0200

    Added Fox 3D model to Contact page

---
+++ src/assets/fox/Scene.jsx
@@ -0,0 +1,56 @@
+import React, { useRef, useEffect } from "react";
+import scenePath from "./scene-transformed.glb";
+import { useGLTF, useAnimations } from "@react-three/drei";
+import useGUI from "../../useGUI";
+
+export function Fox(props) {
+  // 3D Model from: https://sketchfab.com/3d-models/fox-f372c04de44640fbb6a4f9e4e5845c78
+  const group = useRef();
+  const { nodes, materials, animations } = useGLTF(scenePath);
+  const { actions } = useAnimations(animations, group);
+
+  // This effect will run when the component mounts, and it will start the "Flying" animation
+  useEffect(() => {
+    actions.hit.play();
+  }, [actions]);
+
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group name="Sketchfab_Scene">
+        <primitive object={nodes.GLTF_created_0_rootJoint} />
+        <skinnedMesh
+          name="Object_7"
+          geometry={nodes.Object_7.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Object_7.skeleton}
+        />
+        <skinnedMesh
+          name="Object_8"
+          geometry={nodes.Object_8.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Object_8.skeleton}
+        />
+        <skinnedMesh
+          name="Object_9"
+          geometry={nodes.Object_9.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Object_9.skeleton}
+        />
+        <skinnedMesh
+          name="Object_10"
+          geometry={nodes.Object_10.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Object_10.skeleton}
+        />
+        <skinnedMesh
+          name="Object_11"
+          geometry={nodes.Object_11.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Object_11.skeleton}
+        />
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload(scenePath);
